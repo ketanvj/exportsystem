fed0cd2fb1cc8fc61c76ef74d44364c3116936c7
ketanvj <ketanvj@yahoo.com>
2022-01-18 11:05:15 +0530

    Final changes into the system with v1.0

--- dashboard_chart_field.py
+++ dashboard_chart_field.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2020, Frappe Technologies and contributors
 # License: MIT. See LICENSE
-
+# added for comments
+# added one more comment
 # import frappe
 from frappe.model.document import Document
 
